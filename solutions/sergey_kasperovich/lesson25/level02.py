859c95afee9f47bddccbb4d1da56b175afc2096e
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-06-29 21:59:09 +0300

    Sergeyk (#14)
    * 11
    
    * 11
    
    * lesson25
    
    * pipenv
    
    * lesson2

---
+++ solutions/sergey_kasperovich/lesson25/level02.py
@@ -0,0 +1,7 @@
+from solutions.sergey_kasperovich.lesson25.level01 import User as UserValue
+
+
+class User(UserValue):
+    def __init__(self, name, mail):
+        self.name = name
+        self.mail = mail
