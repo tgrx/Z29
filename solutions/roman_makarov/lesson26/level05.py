c3997755d506d79bbc19607c9d8e2708955c4a66
makarr-off <makarr-off@yandex.ru>
2020-07-13 14:30:18 +0300

    26.level05

---
+++ solutions/roman_makarov/lesson26/level05.py
@@ -0,0 +1,12 @@
+from project.consts import COINAGE
+
+
+def get_change(amount):
+    bills = {}
+    amount *= 100
+    for note, cost in sorted(COINAGE.items(), key=lambda i: -i[1]):
+        number = int(amount // cost)
+        bills[note] = number
+        amount -= cost*number
+
+    return bills
